b73abd32075ced8a86a80191579b597a4daa2c31
trystep <alexeevmakc@mail.ru>
2020-08-08 21:44:01 +0300

    added temp;ates

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Content
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Content.objects.order_by('id')[:5]
+    template = loader.get_template('app/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    question_detail = Content.objects.get(pk=question_id)
+    template = loader.get_template('app/detail.html')
+    context = {
+        'question_detail': question_detail,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def get_question(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
