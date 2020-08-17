27c49ba77ad1d2bc5e9451c1539f044aa049a172
trystep <alexeevmakc@mail.ru>
2020-08-17 17:52:58 +0300

    added robots.txt

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from django.views.generic.base import TemplateView
 
 app_name = 'app'
 urlpatterns = [
@@ -12,5 +13,9 @@ urlpatterns = [
     path('all/', views.AllView.as_view()),
     # ex: /results/<>
     path('search/', views.SearchView.as_view(), name='search_results'),
-
+    # Эта строка требуется для отображения robots.txt по адресу .../robots.txt
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
